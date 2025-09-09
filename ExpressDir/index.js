f3b9b95055e4010f1fc978220541d1cad06328a6
Rahul Heer <rahulheer344@gmail.com>
2025-09-09 11:56:47 +0530

    sending response

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 
 const app = express()
-let port = 3000;
+let port = 8080;
 
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}`)
 })
+ app.use((req,res)=>{
+    res.send("<h1>Hello World</h1>");
+ })
+
+module.exports = app;
