ee281ec3fdcc1f2c86925838322db62ce75972d7
Rahul Heer <rahulheer344@gmail.com>
2025-09-09 11:31:04 +0530

    mian

---
+++ math.js
@@ -0,0 +1,13 @@
+const sum=(a,b)=> (a+b);
+const mult=(a,b)=> (a*b);
+const pi=3.14;
+const square=(a)=> (a*a);
+
+let obj={
+    sum:sum,
+    mult:mult,
+    pi:pi,
+    square:square
+}
+module.exports=obj;
+
---
+++ script.js
@@ -0,0 +1,6 @@
+const math=require('./math');
+console.log(math.square(-4));
+console.log(math.sum(3,5));
+console.log(math.mult(3,5));
+console.log(math.pi);
+
