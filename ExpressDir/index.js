fcb6c237c7742d01f9754a408d72969c8b03951f
Rahul Heer <rahulheer344@gmail.com>
2025-09-09 11:34:45 +0530

    chnaging express setup

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -4,9 +4,6 @@ const app = express()
 console.dir(app);
 let port = 3000;
 
- app.get('/', (req, res) => {
-   res.send('Hello World')
- })
 
 app.listen(port, () => {
   console.log(` app listening on port ${port}`)
