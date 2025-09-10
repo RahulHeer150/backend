110291bae81462c2de95d0e6115d86a3cf9819bb
Rahul Heer <rahulheer344@gmail.com>
2025-09-10 15:00:54 +0530

    adding new ejs files

--- EJS_Dir/index.js
+++ EJS_Dir/index.js
@@ -5,7 +5,7 @@ let port = 8080;
 app.set('view engine', 'ejs'); // setting ejs as view engine
 
 app.get('/', (req, res) => {
-    res.send("this is home"); // rendering index.ejs file
+    res.send("this is homepage"); // rendering index.ejs file
 });
 app.listen(port, () => {
     console.log(` app listening on port ${port}`)
