726c6782ae7d5c2e709f7740d1b86bb7a1872ab7
Rahul Heer <rahulheer344@gmail.com>
2025-09-13 23:37:35 +0530

    new thing

--- get-post/Backend/index.js
+++ get-post/Backend/index.js
@@ -5,7 +5,7 @@ const port = 3000
 app.get('/register', (req, res) => 
     res.send('Hello World!'));
 app.post('/register',(req,res)=>{
-    res.send("data is sucessful")
+    res.send("data is sucessful");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
