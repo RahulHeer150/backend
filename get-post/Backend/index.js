09c5ca43a854ffe421dab8f619f0d831876350ff
Rahul Heer <rahulheer344@gmail.com>
2025-09-14 17:05:16 +0530

    new changes

--- get-post/Backend/index.js
+++ get-post/Backend/index.js
@@ -1,9 +1,10 @@
  const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
-app.get('/register', (req, res) => 
-    res.send('Hello World!'));
+app.get("/register", (req, res) =>{
+    res.send('Hello World!')
+});
 app.post('/register',(req,res)=>{
     res.send("data is sucessful");
 });
