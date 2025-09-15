44d4052803a8d611480e09029b5057a21fd55b56
Rahul Heer <rahulheer344@gmail.com>
2025-09-15 20:17:45 +0530

    adding chrome content

--- get-post/Frontend/app.js
+++ get-post/Frontend/app.js
@@ -7,4 +7,9 @@ function personMaker(name, age) { //factory function
     }
   };
   return person;
-}
+} 
+
+let p1=personMaker("Alice", 30);
+let p2=personMaker("Bob", 25);
+p1.greet(); // Hello, my name is Alice and I am 30 years old.
+p2.greet(); // Hello, my name is Bob and I am 25 years old.
