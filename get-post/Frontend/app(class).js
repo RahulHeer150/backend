07499081f5beb5bd5990898ef89314858506c39c
Rahul Heer <rahulheer344@gmail.com>
2025-09-15 21:04:58 +0530

    clases in js

--- get-post/Frontend/app(class).js
+++ get-post/Frontend/app(class).js
@@ -4,4 +4,9 @@ class Person { //constructor function
    this.age=age;
 
 }
+talk(){
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
+}
+let p1= new Person("Alice", 30);
+let p2=new Person("Bob", 25);
