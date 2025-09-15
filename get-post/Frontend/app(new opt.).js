b92d005456e6f3a200eeb93068095b206bb4831d
Rahul Heer <rahulheer344@gmail.com>
2025-09-15 20:31:10 +0530

    new code

--- get-post/Frontend/app(new opt.).js
+++ get-post/Frontend/app(new opt.).js
@@ -4,8 +4,8 @@ function Person(name, age) { //constructor function
    this.name=name,
    this.age=age
 } 
-
-let p1=Person("Alice", 30);
-let p2=Person("Bob", 25);
-p1.greet(); // Hello, my name is Alice and I am 30 years old.
-p2.greet(); // Hello, my name is Bob and I am 25 years old.
+Person.prototype.talk=function(){
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+}
+let p1= new Person("Alice", 30);
+let p2=new Person("Bob", 25);
