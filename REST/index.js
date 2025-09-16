b3295cb851b4d195d8e8cbebc7d9110f088da255
Rahul Heer <rahulheer344@gmail.com>
2025-09-16 13:00:31 +0530

    new content for a blog app

--- REST/index.js
+++ REST/index.js
@@ -8,8 +8,34 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs') // Set EJS as the templating engine
 // Define a route that renders an EJS template
 //
-app.set(express.static(path.join(__dirname, 'views')))
-app.get('/', (req, res) =>{
-     res.send('Hello World!')
+app.use(express.static(path.join(__dirname, 'public')))
+
+let posts=[
+    {
+        username:"rahul",
+        content:"hello this is my first post"
+    },
+    {
+        username:"john",
+        content:"hello this is my second post"
+    },
+     {
+        username:"john",
+        content:"hello this is my first internship"
+    },
+]
+
+app.get('/posts', (req, res) =>{
+     res.render('index.ejs',{posts})
+});
+app.get('/posts/new',(req,res)=>{
+    res.render('new.ejs');
+});
+app.post('/posts',(req,res)=>{
+   let {username,content}=req.body;
+   posts.push({username,content});
+   console.log(posts); // shows posts in server console
+    res.send("form submitted");
+    
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
