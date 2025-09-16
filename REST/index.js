8ac304bbd7841a04728fe55f5dd3fce338ee87a4
Rahul Heer <rahulheer344@gmail.com>
2025-09-16 10:28:31 +0530

    new code for rest api

--- REST/index.js
+++ REST/index.js
@@ -1,6 +1,15 @@
  const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+const path = require('path')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs') // Set EJS as the templating engine
+// Define a route that renders an EJS template
+//
+app.set(express.static(path.join(__dirname, 'views')))
+app.get('/', (req, res) =>{
+     res.send('Hello World!')
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
