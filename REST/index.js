b2141f0a8dc6a8606d8973e1a4587bfe88198572
Rahul Heer <rahulheer344@gmail.com>
2025-09-17 20:29:39 +0530

    final commit

--- REST/index.js
+++ REST/index.js
@@ -62,7 +62,13 @@ app.get('/posts/:id', (req, res) =>{
    res.render('show.ejs',{post})
 });
 
- 
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+   posts=posts.filter((p)=>id !==p.id);
+   res.redirect('/posts')
+}
+);
+
 app.post('/posts',(req,res)=>{ 
    let {username,content}=req.body;
    let id=uuidv4();
