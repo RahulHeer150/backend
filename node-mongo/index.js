3ab3e03acf01a5cef80abbaf295201ee508009d3
Rahul Heer <rahulheer344@gmail.com>
2025-09-26 11:52:20 +0530

    new code changes for basuc function of mongodb

---
+++ node-mongo/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+// let url='http://localhost:8080/users ';
+
+async function connectToDatabase() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/mydatabase');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+}); 
+const User = mongoose.model('User', userSchema);
+
+connectToDatabase();
